d76669c0aad222788ca9a9d98375270d6ac06dba
thor <thor@cphbusiness.dk>
2023-01-05 16:54:50 +0100

    add plot

--- sh2.R
+++ sh2.R
@@ -6,6 +6,15 @@ library(DT)
 library(shinycssloaders)
 library(shinydashboard)
 
+dat <- readRDS("data/subregion_agg.rds")
+clean_dat <- dat %>% 
+  select(!subregion1_name) %>% 
+  filter(country_name=="Canada" & date >= "2020-01-01" & date <= "2020-12-31") %>% 
+  group_by(country_name, date) %>% 
+  summarise_all(sum) %>% 
+  select( country_name, date, "new_confirmed") %>% 
+  arrange(date)
+
 
 #ui <- fluidPage(
 ui <- dashboardPage(
@@ -39,6 +48,10 @@ ui <- dashboardPage(
 )
 
 server <- function(input,output,session) {
+  
+  ggplot (data=clean_dat, aes(y=new_confirmed,x=date, color=country_name))+
+    geom_line(size=1.5) +
+    labs(color="Country Name")
   
 }
 
